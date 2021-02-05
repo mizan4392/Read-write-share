7a55b2e62074d98b2c65499549ce85522ed55a92
mizan <dev.mizan4392@gmail.com>
2021-02-05 17:36:38 +0600

    added login & signup Ui

--- ui/src/pages/login/Login.page.tsx
+++ ui/src/pages/login/Login.page.tsx
@@ -1,7 +1,68 @@
+import { KeyOutlined, MailOutlined } from '@ant-design/icons'
+import { Button, Col, Form, Input, Row } from 'antd'
 import React from 'react'
 import './login.css'
 
 export const Login: React.FC = () => {
-    return <div>Login</div>
-  }
+  return <div className="login"><h2>Read Write & Share</h2>
+    <div style={{ background: "#fff", padding: "20px" }}>
+      <Form name="login" >
+        <Form.Item
+          style={{ width: "400px" }}
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username or email!",
+            },
+          ]}
+        >
+          <Input
+            name="email"
+            type="email"
+            style={{ width: "400px" }}
+            prefix={<MailOutlined />}
+            placeholder="Email"
+            size="large"
+          />
+        </Form.Item>
+
+        <Form.Item
+          style={{ width: "400px" }}
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input
+            style={{ width: "400px" }}
+            prefix={<KeyOutlined />}
+            type="password"
+            name="password"
+            placeholder="Password"
+            size="large"
+          />
+        </Form.Item>
+
+        <Row justify="space-between">
+          <Col></Col>
+          <Col>
+            <Button
+              htmlType="submit"
+              style={{ color: "#000" }}
+            // loading={loginLoading}
+            >
+              Login
+            </Button>
+          </Col>
+        </Row>
+        <div className="form-group">
+          <a
+            href="/signup"
+          // onClick={() => this.props.history.push("/signup")}
+          >
+            Don't have an Account? Signup
+          </a>
+        </div>
+      </Form>
+    </div></div>
+}
 
